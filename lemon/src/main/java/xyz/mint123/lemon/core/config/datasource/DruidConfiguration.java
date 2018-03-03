ee32b682368aaa231fc3560e43f4f42ac1e407de
lemon <lemon_yp@hotmail.com>
2018-03-03 10:46:57 +0800

    datasource

--- lemon/src/main/java/xyz/mint123/lemon/core/config/datasource/DruidConfiguration.java
+++ lemon/src/main/java/xyz/mint123/lemon/core/config/datasource/DruidConfiguration.java
@@ -1,25 +1,6 @@
 package xyz.mint123.lemon.core.config.datasource;
 
-import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
-
-import com.alibaba.druid.spring.boot.autoconfigure.DruidDataSourceBuilder;
-import com.alibaba.druid.support.http.StatViewServlet;
-import com.alibaba.druid.support.http.WebStatFilter;
-import org.mybatis.spring.SqlSessionFactoryBean;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
-import org.springframework.boot.web.servlet.ServletRegistrationBean;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import com.alibaba.druid.filter.Filter;
-import com.alibaba.druid.filter.stat.StatFilter;
-import com.alibaba.druid.pool.DruidDataSource;
-import org.springframework.context.annotation.Primary;
-import xyz.mint123.lemon.core.support.datasource.RoutingDataSource;
 
 import javax.sql.DataSource;
 
