e76ff3e9c5d2d5594acecf1658fb190b752d564d
Mint <lemon_yp@hotmail.com>
2018-04-08 01:35:16 +0800

    加入模板引擎

---
+++ lemon/src/main/java/xyz/mint123/lemon/sys/web/SystemController.java
@@ -0,0 +1,39 @@
+package xyz.mint123.lemon.sys.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import xyz.mint123.lemon.core.base.BaseController;
+
+/**
+ * 系统控制器
+ * @author lemon
+ */
+@Controller
+@RequestMapping("/")
+public class SystemController extends BaseController {
+
+    /**
+     * 首页
+     * @return
+     */
+    @GetMapping({"/","index","index.html"})
+    public String index(ModelMap modelMap){
+        return "sys/index";
+    }
+
+    /**
+     * 登录页面
+     * @param modelMap
+     * @return
+     */
+    @GetMapping("/login.html")
+    public String login(ModelMap modelMap){
+        return "sys/login";
+    }
+
+
+
+
+}
